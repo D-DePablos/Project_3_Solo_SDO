a63f7dbdf0d37801a7db4922c606e35c5ba4d91a
D-DePablos <d.depablosaguero@gmail.com>
2021-03-09 17:52:26 +0000

    Fixing Lightcurve Script

--- Scripts/Solar/lightCurves.py
+++ Scripts/Solar/lightCurves.py
@@ -2,11 +2,15 @@ def getRemoteData():
     """
     Gets the remote data using remoteData AIAManager class
     """
-    from Solar.remoteData import SDOAIAManager
+    from .remoteData import SDOAIAManager
     from astropy import units as u
 
     sdoaia = SDOAIAManager(aia=SDOAIAManager(times=("2020/5/27",
                                                     "2020/5/28 14:00"),
                                              cadence=1 * u.minute,
-                                             aiaPath=""))
+                                             aiaPath="/disk/solar18/ddp/PhD/Project_3_Solo_SDO/unsafe/remoteData/AIA"))
     sdoaia.downloadData(force=True)
+
+
+if __name__ == "__main__":
+    getRemoteData()
